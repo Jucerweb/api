0e5ef9fe96843997f5f61b3807ca0d19b86c2507
Julio Orozco <kommbea@Kommbeas-MacBook-Pro.local>
2015-05-04 11:20:12 -0500

    Finish Api SoundCloud

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,25 +1,29 @@
-SC.initialize({
-  client_id:"0e43f3b794fdcc9ee9b17298c096fe7a"
-});
-
+// $(function(){ igual a document.ready
 $(document).ready(function() {
-	$("#search").keypress(function(){
-		
+	SC.initialize({
+  	client_id:"0e43f3b794fdcc9ee9b17298c096fe7a"
+	});
+	// $('#search').autocomplete({
+	// 	source: function(req, res) {
+	// 		SC.get('/tracks', { limit: 10, q: req.term}, function(tracks) {
+	// 			$.each(tracks, function(i, track){
+	// 				track.label = track.title;
+	// 			});
+	// 			res(tracks);
+	// 		});
+	// 	},
+	// 	select: function(e, ui) {
+	// 		SC.oEmbed(ui.item.permalink_url, document.getElementById('get'));
+	// 	}
+	// });
+	$("#search").keyup(function(e){
 		var Data = $("#search").val();
-		$('#get').html('');
-		//if (Data.length > 1)	
-				SC.get('/tracks', { limit: 5 , q: Data}, function(tracks) {
-					var id = 1;
-					$(tracks).each(function(index, track) {
-				
-						//$('div').append($('<li class="results"></li>').html(track.title));
-						$('#get').append('<div class="list-group-item" id="'+ id +'">' + '<a href="' + track.permalink_url + '">' + track.title + '</a>'+ '</div><br>');
-						  SC.oEmbed(track.permalink_url, document.getElementById(id));
-						id ++;
-					});
-				});
-		//else
-			//$('.results').remove();
-		
+		SC.get('/tracks', { limit: 10, q: Data}, function(tracks) {
+			$('#get').html('');
+			$(tracks).each(function(index, track) {
+				$('#get').append('<div class="list-group-item" id="'+ track.id +'">' + '<a href="' + track.permalink_url + '">' + track.title + '</a>'+ '</div><br>');
+				SC.oEmbed(track.permalink_url, document.getElementById(track.id));
+			});
+		});
 	});
 });
